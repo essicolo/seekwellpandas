a7a314e551f3fa7f6dfa768ee4819ec62bf8160b
Essi Parent <9ivko1ozf@relay.firefox.com>
2024-09-26 17:12:18 -0400

    basic docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 project = 'pandalchemy'
 copyright = '2024, Essi Parent'
 author = 'Essi Parent'
@@ -14,7 +18,10 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'nbsphinx'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
