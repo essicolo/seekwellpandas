1724dce5e98b82e10976b37c65a08a5aaea45c73
Essi Parent <9ivko1ozf@relay.firefox.com>
2024-09-26 17:31:16 -0400

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
@@ -16,21 +8,18 @@ copyright = '2024, Essi Parent'
 author = 'Essi Parent'
 release = '0.1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',
-    'nbsphinx'
+    'sphinx.ext.napoleon',
+    'nbsphinx',.
 ]
+
+master_doc = 'index'
+source_suffix = ['.rst', '.md']
+htmlhelp_basename = 'PandalchemyDoc'
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,23 +1,14 @@
-.. pandalchemy documentation master file, created by
-   sphinx-quickstart on Thu Sep 26 16:50:35 2024.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-pandalchemy documentation
-=========================
-
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
-
+Welcome to Pandalchemy's documentation!
+=======================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+   installation
+   usage
    api
-   _examples/basic-usage
-
+   examples/basic-usage
 
 Indices and tables
 ==================
