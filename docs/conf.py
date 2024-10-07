083031a3b416a13f22e80ec3882a1e210faf69c9
Essi Parent <9ivko1ozf@relay.firefox.com>
2024-10-07 11:36:01 -0400

    0.2.4

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ print("Python path:", sys.path)
 project = 'seekwellpandas'
 copyright = '2024, Essi Parent'
 author = 'Essi Parent'
-release = '0.2.3'
+release = '0.2.4'
 
 # Extensions Sphinx
 extensions = [
